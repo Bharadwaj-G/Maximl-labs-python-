aa5af0a8ab30c56774cb2d9aceff2e8b8fff5c17
Bharadwaj Gottumukkala <44552452+Bharadwaj-G@users.noreply.github.com>
2020-11-03 13:46:47 +0530

    Update substring.py

--- substring.py
+++ substring.py
@@ -1,24 +1,24 @@
 #max_sistinct_char() -  function to return the max distinct characters in a string
-def max_distinct_char(str): 
-	distinct_char = set(str)
+def max_distinct_char(S): 
+	distinct_char = set(S)
 	max_distinct_char = len(distinct_char)	
 	return max_distinct_char
 
 #substrings() - function to return all the substrings as a list
-def substrings(string,n):
+def substrings(S,n):
 	l1 = [] 
 	for i in range(n): 
 		for j in range(i+1,n+1): 
-			substring = string[i:j] 
+			substring = S[i:j] 	
 			l1.append(substring)
 	return(l1)
 
 #smallestSubstring()  -  function to return the smallest substring with max distinct characters
-def smallestSubstring(string): 
-	n = len(string)	 
-	max_distinct = max_distinct_char(string)    
+def smallestSubstring(S): 
+	n = len(S)	 
+	max_distinct = max_distinct_char(S)    
 	min_length = n	 
-	substring_list = substrings(string,n)
+	substring_list = substrings(S,n)
 	for i in range(0,len(substring_list)):
 		length = len(substring_list[i]) 
 		distinct_char = max_distinct_char(substring_list[i])           #to find distinct characters in substring
@@ -27,7 +27,7 @@ def smallestSubstring(string):
 	return min_length
 
 if __name__ == "__main__":  
-	string = input()
-	l = smallestSubstring(string); 
+	S = input()
+	l = smallestSubstring(S); 
 	print(l) 
 
